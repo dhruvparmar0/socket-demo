5cbc29f42621c1375da13d6a4555d02dac3f3391
Dhruv parmar <dhruv.parmar@vihaainfotech.in>
2025-06-18 20:25:41 +0530

    Initial working socket demo

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.of("/TeenPatti").on('connection', (socket) => {
+  console.log(`🟢 Unity Connected: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`🔴 Disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('🚀 Server started on port 3000');
+});
